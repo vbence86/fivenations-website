a227422ca0f49fa41ff1c43cd64a2799b12a65e3
Bence Varga <vbence86@gmail.com>
2017-06-05 18:38:19 +0100

    FeatureList

--- src/landingpage/components/FeatureList.js
+++ src/landingpage/components/FeatureList.js
@@ -5,6 +5,19 @@ export default class FeatureList extends Component {
   constructor(props) {
     super(props);
     this.contentProvider = this.props.contentProvider;
+    this.state = this.contentProvider.get('landingPage').featureList;
+  }
+
+  renderFeatureItems() {
+    return this.state.featureItems.map( (v, i) => (
+      <div className="col-sm-4 col-xs-6" key={i}>
+        <div className="block">
+          <i className={`fa fa-${v.icon} fa-3x`}></i>
+          <h4 className="text-uppercase">{v.header}</h4>
+          <p>{v.description}</p>
+        </div>
+      </div>
+    ));
   }
 
   render() {
@@ -13,65 +26,12 @@ export default class FeatureList extends Component {
         <div className="row">
           <div className="overlay"></div>
           <div className="col-sm-12">
-            <h3 className="text-center"><i className="fa fa-bookmark"></i>What I Do</h3>
+            <h3 className="text-center"><i className="fa fa-bookmark"></i>{this.state.header}</h3>
           </div>
         </div>
         <section className="container">
           <div className="row">
-            <div className="col-sm-4 col-xs-6">
-              <div className="block">
-                <i className="fa fa-android fa-3x"></i>
-                <h4 className="text-uppercase">Mobile Apps</h4>
-                <p>
-                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.
-                </p>
-              </div>
-            </div>
-            <div className="col-sm-4 col-xs-6">
-              <div className="block">
-                <i className="fa fa-code fa-3x"></i>
-                <h4 className="text-uppercase">web design</h4>
-                <p>
-                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.
-                </p>
-              </div>
-            </div>
-            <div className="col-sm-4 col-xs-6">
-              <div className="block">
-                <i className="fa fa-magic fa-3x"></i>
-                <h4 className="text-uppercase">UX/UI Design</h4>
-                <p>
-                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.
-                </p>
-              </div>
-            </div>
-            <div className="col-sm-4 col-xs-6">
-              <div className="block">
-                <i className="fa fa-camera fa-3x"></i>
-                <h4 className="text-uppercase">Photography</h4>
-                <p>
-                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.
-                </p>
-              </div>
-            </div>
-            <div className="col-sm-4 col-xs-6">
-              <div className="block">
-                <i className="fa fa-lightbulb-o fa-3x"></i>
-                <h4 className="text-uppercase">brand identity</h4>
-                <p>
-                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.
-                </p>
-              </div>
-            </div>
-            <div className="col-sm-4 col-xs-6">
-              <div className="block">
-                <i className="fa fa-user-secret fa-3x"></i>
-                <h4 className="text-uppercase">analytics</h4>
-                <p>
-                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.
-                </p>
-              </div>
-            </div>
+            {this.renderFeatureItems()}
           </div>
         </section>
       </section>
--- src/landingpage/components/LandingPage.js
+++ src/landingpage/components/LandingPage.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import Universe from './Universe';
 import Header from './Header';
 import ContentMenu from './ContentMenu';
-import Carousel from './Carousel';
 import AboutUs from './AboutUs';
 import FeatureList from './FeatureList';
 import CTABox from './CTABox';
@@ -16,11 +15,10 @@ export class LandingPage extends Component {
       <Universe>
         <Header contentProvider={ContentProvider} />
         <ContentMenu contentProvider={ContentProvider} />
-        <Carousel contentProvider={ContentProvider} />
-        <AboutUs contentProvider={ContentProvider} />
         <FeatureList contentProvider={ContentProvider} />
         <CTABox contentProvider={ContentProvider} />
         <ContactUs contentProvider={ContentProvider} />
+        <AboutUs contentProvider={ContentProvider} />
       </Universe>
     );
   }

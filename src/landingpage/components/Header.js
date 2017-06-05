620d6ca5e5edfca4999924d61c8a1e89a7b5aa50
Bence Varga <vbence86@gmail.com>
2017-06-05 15:07:26 +0100

    connect Header compoment to Contentful content

--- src/landingpage/components/Header.js
+++ src/landingpage/components/Header.js
@@ -5,6 +5,24 @@ export default class Header extends Component {
   constructor(props) {
     super(props);
     this.contentProvider = this.props.contentProvider;
+    this.state = this.contentProvider.get('landingPage');
+  }
+
+  renderMenu() {
+    return this.state.headerNavigation.map( (v, i) => (
+      <li role="presentation" key={i}><a href={v.href}>{v.label}</a></li>
+    ));
+  }
+
+  renderSocialIcons() {
+    const icons = this.state.socialProviders.map( (v, i) => (
+      <li key={i}><a href={v.url} className={`icon-${v.socialProviderId}`}>{v.label}</a></li>
+    ));
+    return (
+      <ul className="social-networks spin-icon">
+        {icons}
+      </ul>
+    );
   }
 
   render() {
@@ -13,28 +31,18 @@ export default class Header extends Component {
           <div className="row">
             <div className="overlay" />
             <ul className="nav nav-pills menu-center margin-top-30 scroll-nav">
-              <li role="presentation" className="active"><a href="#home"><i className="fa fa-home">{this.contentProvider.get('landingPage').pageTitle}</i></a></li>
-              <li role="presentation"><a href="#portfolio"></a></li>
-              <li role="presentation"><a href="#reviews">Reviews</a></li>
-              <li role="presentation"><a href="#about_me">About</a></li>
-              <li role="presentation"><a href="#expertness">Services</a></li>
-              <li role="presentation"><a href="#contact_me">Contact</a></li>
+              <li role="presentation" className="active"><a href="#home"><i className="fa fa-home">{this.state.pageTitle}</i></a></li>
+              {this.renderMenu()}
             </ul>
             <img src="images/profile.jpg" className="margin-top-76 img-circle center-block profile_picture" alt="profile" />
             <h1 className="text-capitalize text-center">Jeanetter benson</h1>
             <h3 className="text-capitalize text-center">UX/UI designer</h3>
             <hr/>
             <div className="text-center margin-bottom-135">
-              <ul className="social-networks spin-icon">
-                <li><a href="https://www.facebook.com/" className="icon-facebook">Facebook</a></li>
-                <li><a href="https://twitter.com/" className="icon-twitter">Twitter</a></li>
-                <li><a href="https://www.linkedin.com/" className="icon-linkedin">LinkedIn</a></li>
-                <li><a href="https://pinterest.com" className="icon-pinterest">Pinterest</a></li>
-                <li><a href="https://instagram.com" className="icon-instagram">Instagram</a></li>
-              </ul>
+              {this.renderSocialIcons()}
             </div>
           </div>
-        </header>      
+        </header>
     );
   }
 

b9478adeba988aa510d57a5c46c2993017b745b2
Bence Varga <vbence86@gmail.com>
2017-06-09 23:28:27 +0100

    CTABox is linked to Contentful

--- src/landingpage/components/CTABox.js
+++ src/landingpage/components/CTABox.js
@@ -5,6 +5,7 @@ export default class CTABox extends Component {
   constructor(props) {
     super(props);
     this.contentProvider = this.props.contentProvider;
+    this.state = this.contentProvider.get('landingPage').ctaBox;
   }
 
   render() {
@@ -12,11 +13,11 @@ export default class CTABox extends Component {
       <section className="container" id="hire_me">
         <div className="row">
           <div className="col-sm-12 text-center">
-            <h3>Need Any Help?</h3>
-            <h4><i className="fa fa-thumbs-up fa-2x"></i> I am available for freelance hire</h4>
+            <h3>{this.state.header}</h3>
+            <h4><i className="fa fa-thumbs-up fa-2x"></i>{this.state.subtitle}</h4>
 
             <nav className="cl-effect-2">
-              <a href="#contact_me"><span data-hover="Hire Me">Hire Me</span></a>
+              <a href="#contact_me"><span data-hover="{this.state.buttonLabel}">{this.state.buttonLabel}</span></a>
             </nav>
           </div>
         </div>

0403e4962fd3f45a428e4f7bb887d2c9288c29bd
Bence Varga <vbence86@gmail.com>
2017-06-11 13:59:34 +0100

    add About Us basic content

--- src/landingpage/components/AboutUs.js
+++ src/landingpage/components/AboutUs.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ReactMarkdown from 'react-markdown';
 
 export default class AboutUs extends Component {
 
@@ -15,14 +16,9 @@ export default class AboutUs extends Component {
           <div className="col-md-10 col-md-offset-1 col-sm-12">
             <div className="row">
               <div className="col-md-5 col-sm-4 col-xs-12">
-                <h3><i className="fa fa-user"></i>About me</h3>
+                <h3><i className="fa fa-user"></i>{this.state.aboutUsHeader}</h3>
                 <div className="block">
-                  <p>
-                    I am a passionate User Experience and User Interface Designer who has been working in this field for more than 3 years now.
-                  </p>
-                  <p>
-                    You have an idea, you need someone to bring it ot life. Or you already have a product that needs to be redined to meet the demand of 2017 users!. This is where i come in! With my knowledge and creativity. I can take your idea, your product to the next level. Lets have a chat and discuss your idea!
-                  </p>
+                  <ReactMarkdown source={this.state.aboutUsContent} />
                 </div>
               </div>
               <div className="col-md-3 col-sm-4 col-xs-6">

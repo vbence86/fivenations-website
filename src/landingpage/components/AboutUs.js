b34909fff6ec7973e61608fddf1c14ce49426077
Bence Varga <vbence86@gmail.com>
2017-06-12 17:10:32 +0100

    Finalise AboutUs section

--- src/landingpage/components/AboutUs.js
+++ src/landingpage/components/AboutUs.js
@@ -1,12 +1,80 @@
 import React, {Component} from 'react';
 import ReactMarkdown from 'react-markdown';
 
+class AboutUsSection extends Component {
+
+  render() {
+    return (
+      <div className="col-md-5 col-sm-4 col-xs-12">
+        <h3><i className="fa fa-user"></i>{this.props.header}</h3>
+        <div className="block">
+          <ReactMarkdown source={this.props.content} />
+        </div>
+      </div>      
+    );    
+  }  
+
+}
+
+class TeamMembersSection extends Component {
+  
+  render() {
+    return (
+      <div className="col-md-3 col-sm-4 col-xs-6">
+        <h3><i className="fa fa-building"></i>{this.props.content.header}</h3>
+        {this.renderTeamMembers()}
+      </div>
+    );
+  }
+
+  renderTeamMembers() {
+    return this.props.content.teamMembers.map( (v, i) => {
+      const twitter = v.twitter && v.twitter.split('/').pop();
+      return (
+        <div className="block" key={i}>
+          <h4>{v.name}</h4>
+          <ul className="icon-list">
+            <li><i className="fa fa-tag"></i> {v.title}</li>
+            <li><i className="fa fa-google"></i><a href="mailto:{v.email}"> {v.email}</a></li>
+            <li><i className="fa fa-twitter"></i><a href="{v.twitter}"> {twitter}</a></li>
+          </ul>
+        </div>
+      );
+    });
+  }
+
+}
+
+class OpenPoisitionsSection extends Component {
+
+  render() {
+    return (
+      <div className="col-md-4 col-sm-4 col-xs-6">
+        <h3><i className="fa fa-graduation-cap"></i>{this.props.content.header}</h3>
+        {this.renderOpenPositions()}
+      </div>      
+    );    
+  }
+
+  renderOpenPositions() {
+    return this.props.content.positions.map( (v, i) => (
+      <div className="block">
+        <h4>{v.title}</h4>
+        <ul className="icon-list">
+          <li><i className="fa fa-user-circle"></i> {v.description}</li>
+        </ul>
+      </div>
+    ));
+  }
+
+}
+
 export default class AboutUs extends Component {
 
   constructor(props) {
     super(props);
     this.contentProvider = this.props.contentProvider;
-    this.state = this.contentProvider.get('landingPage').aboutUs;  
+    this.state = this.contentProvider.get('landingPage').aboutUs;
   }
 
   render() {
@@ -15,61 +83,9 @@ export default class AboutUs extends Component {
         <div className="row">
           <div className="col-md-10 col-md-offset-1 col-sm-12">
             <div className="row">
-              <div className="col-md-5 col-sm-4 col-xs-12">
-                <h3><i className="fa fa-user"></i>{this.state.aboutUsHeader}</h3>
-                <div className="block">
-                  <ReactMarkdown source={this.state.aboutUsContent} />
-                </div>
-              </div>
-              <div className="col-md-3 col-sm-4 col-xs-6">
-                <h3><i className="fa fa-building"></i>Work history</h3>
-                <div className="block">
-                  <h4>Product Manager</h4>
-                  <ul className="icon-list">
-                    <li><i className="fa fa-building"></i> Dropbox</li>
-                    <li><i className="fa fa-clock-o"></i> 2017-Present</li>
-                  </ul>
-                </div>
-                <div className="block">
-                  <h4> Senior UX Designer</h4>
-                  <ul className="icon-list">
-                    <li><i className="fa fa-building"></i> Dropbox</li>
-                    <li><i className="fa fa-clock-o"></i> 2017-Present</li>
-                  </ul>
-                </div>
-                <div className="block">
-                  <h4>UX Design Intern</h4>
-                  <ul className="icon-list">
-                    <li><i className="fa fa-building"></i> Dropbox</li>
-                    <li><i className="fa fa-clock-o"></i> 2017-Present</li>
-                  </ul>
-                </div>
-              </div>
-              <div className="col-md-4 col-sm-4 col-xs-6">
-                <h3><i className="fa fa-graduation-cap"></i>Education</h3>
-                <div className="block">
-                  <h4>User experience Design</h4>
-                  <ul className="icon-list">
-                    <li><i className="fa fa-book"></i> University of Punjab</li>
-                    <li><i className="fa fa-clock-o"></i> 2017-Present</li>
-                  </ul>
-                </div>
-                <div className="block">
-                  <h4>User experience Design</h4>
-                  <ul className="icon-list">
-                    <li><i className="fa fa-book"></i> University of Punjab</li>
-                    <li><i className="fa fa-clock-o"></i> 2017-Present</li>
-                  </ul>
-                </div>
-                <div className="block">
-                  <h4>User experience Design</h4>
-                  <ul className="icon-list">
-                    <li><i className="fa fa-book"></i> University of Punjab</li>
-                    <li><i className="fa fa-clock-o"></i> 2017-Present</li>
-                  </ul>
-                </div>
-              </div>
-
+              <AboutUsSection header={this.state.aboutUsHeader} content={this.state.aboutUsContent} />
+              <TeamMembersSection content={this.state.teamMembers} />
+              <OpenPoisitionsSection content={this.state.openPositions} />
             </div>
           </div>
         </div>

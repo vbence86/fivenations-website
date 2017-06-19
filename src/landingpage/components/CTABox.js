b13ae5c7ead9b570331f6ec991ca30833b4fe138
Bence Varga <vbence86@gmail.com>
2017-06-19 13:26:24 +0100

    donate button

--- src/landingpage/components/CTABox.js
+++ src/landingpage/components/CTABox.js
@@ -8,6 +8,17 @@ export default class CTABox extends Component {
     this.state = this.contentProvider.get('landingPage').ctaBox;
   }
 
+  renderDonateButton() {
+    return (
+      <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+        <input type="hidden" name="cmd" value="_s-xclick" />
+        <input type="hidden" name="hosted_button_id" value="KN7SL36TVKS9N" />
+        <input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online!" />
+        <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1" />
+      </form>
+    );
+  }
+
   render() {
     return (
       <section className="container" id="support-us">

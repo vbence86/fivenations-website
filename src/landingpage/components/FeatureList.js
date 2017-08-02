016381c0a977eee5de072aa7653351e6462a08f2
vbence86 <vbence86@gmail.com>
2017-08-02 23:21:52 +0200

    change features section layout

--- src/landingpage/components/FeatureList.js
+++ src/landingpage/components/FeatureList.js
@@ -6,13 +6,16 @@ export default class FeatureList extends Component {
     super(props);
     this.contentProvider = this.props.contentProvider;
     this.state = this.contentProvider.get('landingPage').featureList;
+    console.log(this.state);
   }
 
-  renderFeatureItems() {
+  renderFeatureSections() {
     return this.state.featureItems.map( (v, i) => (
-      <div className="col-sm-4 col-xs-6" key={i}>
-        <div className="block">
-          <i className={`fa fa-${v.icon} fa-3x`}></i>
+      <div className="container-fluid FeatureItem" key={i}>
+        <div className="col-sm-5 col-xs-5 col-md-5">
+          <img src={v.image.file.url} alt="animation" />
+        </div>
+        <div className="col-sm-7 col-xs-7 col-md-7">
           <h4 className="text-uppercase">{v.header}</h4>
           <p>{v.description}</p>
         </div>
@@ -21,19 +24,10 @@ export default class FeatureList extends Component {
   }
 
   render() {
+    if (!this.state.featureItems || !this.state.featureItems.length) return null;
     return (
-      <section className="container-fluid sdm-bg" id="features">
-        <div className="row">
-          <div className="overlay"></div>
-          <div className="col-sm-12">
-            <h3 className="text-center"><i className="fa fa-bookmark"></i>{this.state.header}</h3>
-          </div>
-        </div>
-        <section className="container">
-          <div className="row">
-            {this.renderFeatureItems()}
-          </div>
-        </section>
+      <section id="features">
+        {this.renderFeatureSections()}
       </section>
     );
   }

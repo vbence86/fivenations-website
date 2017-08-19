b9f08f2e9e0aebc9235bf51d103ab9937dd5fe36
vbence86 <vbence86@gmail.com>
2017-08-19 15:26:52 +0200

    Add Donately Client to the project

---
+++ src/utils/DonatelyClient.js
@@ -0,0 +1,41 @@
+import Client from './Client';
+
+const serviceEndpoint = 'https://www.dntly.com/api/v1';
+const auth = 'OWExYTgyZjRiYmQ4NDRmMjE0YzRjY2E3MWVlM2NkODU';
+
+const donationsAPICall = `${serviceEndpoint}/donations.json`;
+
+class DonatelyClient extends Client {
+
+  static instance;
+
+  constructor() {
+    super();
+    if (this.instance) return this.instance;
+
+    this.instance = this;
+  }
+
+  donations() {
+    return this.get(donationsAPICall)
+      .then(resp => {
+        return resp.donations;
+      });
+  }
+
+  totalDonations() {
+    return this.donations()
+      .then(donations => {
+        return donations.reduce((sum, donation) => donation.total + sum, 0) || 0;
+      });
+  }
+
+  getHeaders() {
+    return {
+      'Authorization': `Basic ${auth}`, 
+    };
+  }
+
+};
+
+export default DonatelyClient;

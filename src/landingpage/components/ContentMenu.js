2db9468ba2e8dd5a831c3d6c4472ada4914fc777
vbence86 <vbence86@gmail.com>
2017-08-31 18:06:55 +0200

    Add GitHub Star and Watch buttons

--- src/landingpage/components/ContentMenu.js
+++ src/landingpage/components/ContentMenu.js
@@ -4,6 +4,11 @@ import TravisBuilds from './TravisBuilds';
 import GitHubIssues from './GitHubIssues';
 import InstagramPictures from './InstagramPictures';
 
+const gitHubUser = 'vbence86';
+const gitHubRepo = 'fivenations';
+const gitHubStarAPI = `https://ghbtns.com/github-btn.html?user=${gitHubUser}&repo=${gitHubRepo}&type=star&count=true&size=large`;
+const gitHubWatchAPI = `https://ghbtns.com/github-btn.html?user=${gitHubUser}&repo=${gitHubRepo}&type=watch&count=true&size=large&v=2`;
+
 export default class ContentMenu extends Component {
 
   constructor(props) {
@@ -19,6 +24,10 @@ export default class ContentMenu extends Component {
           <h2 className="viewport-animation animated bounceInLeft">{this.state.title}</h2>
           <h4 className="viewport-animation animated bounceInLeft delay-250">{this.state.subtitle}</h4>
           <p className="viewport-animation animated bounceInLeft delay-500">{this.state.description}</p>
+          <div className="viewport-animation animated bounceInLeft delay-500 gitHubStarContainer">
+            <iframe src={gitHubStarAPI} frameborder="0" scrolling="0" width="160px" height="30px"></iframe>
+            <iframe src={gitHubWatchAPI} frameborder="0" scrolling="0" width="160px" height="30px"></iframe>
+          </div>
         </div>
         <Summary contentProvider={this.contentProvider} />
         <div className="col-sm-10 col-sm-offset-1 developmentContainer">

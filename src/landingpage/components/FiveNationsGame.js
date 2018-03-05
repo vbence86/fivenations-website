b0dcbafa2a6b45860a2300967b3975d892f73bf0
Bence Varga <vbence86@gmail.com>
2018-03-05 17:16:25 +0100

    #46

--- src/landingpage/components/FiveNationsGame.js
+++ src/landingpage/components/FiveNationsGame.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 import {demoScenario} from './FiveNationsGameScript';
 
 const gameContainerId = 'fivenations-game';
-const s3BundleUrl = 'https://s3.amazonaws.com/fivenations/fivenations.0.46.0.js';
+const s3BundleUrl = 'https://s3.amazonaws.com/fivenations/fivenations.0.46.1.js';
 
 const getScript = src => {
   return new Promise((resolve, reject) => {

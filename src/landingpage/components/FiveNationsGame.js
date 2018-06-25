04b005e837677e226035060076539950db7d525e
Bence Varga <vbence86@gmail.com>
2018-06-25 12:48:55 +0200

    adjust Five Nations HD version to game popup

--- src/landingpage/components/FiveNationsGame.js
+++ src/landingpage/components/FiveNationsGame.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 import {demoScenario} from './FiveNationsGameScript';
 
 const gameContainerId = 'fivenations-game';
-const s3BundleUrl = 'https://s3.amazonaws.com/fivenations/fivenations.0.55.0.js';
+const s3BundleUrl = 'https://s3.amazonaws.com/fivenations/fivenations.0.61.2.js';
 
 const getScript = src => {
   return new Promise((resolve, reject) => {
